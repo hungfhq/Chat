f1652dbde9a3703614ee533779fa9234ee8a0ded
hunghq <hungfhq@gmail.com>
2020-06-11 16:24:16 +0700

    done Pusher setup

--- resources/js/components/Messages.vue
+++ resources/js/components/Messages.vue
@@ -1,11 +1,25 @@
 <template>
-    <li class="list-group-item"><slot></slot></li>
+    <div>
+      <li class="list-group-item" :class="liClass"><slot></slot></li>
+      <span class="badge float-right" :class="badgeClass">you</span>
+    </div>
 </template>
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
+      props: [
+        'color'
+      ],
+      computed: {
+        liClass() {
+          return 'list-group-item-'+this.color;
+        },
+        badgeClass() {
+          return 'badge-'+this.color;
         }
+      },
+      mounted() {
+          console.log('Messages component mounted.')
+      }
     }
 </script>
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -5,12 +5,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Document</title>
+  <title>Laravel Chat</title>
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <style>
     .list-group {
       overflow-y: scroll;
-      height: 200px;
+      height: 100px;
     }
   </style>
 </head>
@@ -19,8 +19,8 @@
     <div class="row">
       <div class=" col-4">
         <li class="list-group-item active">Chat room</li>
-        <ul class="list-group">
-          <messages v-for="value in chat.message" :key="value.index">
+        <ul class="list-group" v-chat-scroll>
+          <messages v-for="value in chat.message" :key="value.index" color='success'>
             @{{value}}
           </messages>
         </ul>
