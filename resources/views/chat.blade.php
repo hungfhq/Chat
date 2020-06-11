1e36cbbea565d6a5812037db538f69a7c3514cad
hunghq <hungfhq@gmail.com>
2020-06-11 10:55:02 +0700

    init

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <h2></h2>
+      <ul class="list-group col-4">
+        <li class="list-group-item active">Chat room</li>
+        <li class="list-group-item">1</li>
+        <li class="list-group-item">1</li>
+        <li class="list-group-item">1</li>
+        <input type="text" class="form-control" placeholder="type...">
+      </ul>
+    </div>
+  </div>
+  <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
